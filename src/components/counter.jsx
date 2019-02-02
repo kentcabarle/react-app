6ec052e0ce7704c3d9ad07688a6a9b54f6c1f5c4
Kent <kent@Davids-MacBook-Pro.local>
2019-02-02 10:00:10 +0400

    1. renderTags() 2. conditions thruthy

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,35 +7,29 @@ class Counter extends Component {
   // CMD + D = multiple editing
   // <React.Fragment> = avoid duplicate divs
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    count: 1,
+    tags: []
   };
 
-  render() {
-    this.getBadgeClass();
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>No tags!</p>;
+
     return (
-      <React.Fragment>
-        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </React.Fragment>
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
 
-  getBadgeClass() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    // const x = <h1>Zero</h1>;
-    return count === 0 ? "Zero" : count;
+  render() {
+    return (
+      <div>
+        {this.state.tags.length === 0 && "Please create a new tag"}
+        {this.renderTags()}
+      </div>
+    );
   }
 }
 
