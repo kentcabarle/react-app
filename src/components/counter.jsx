349269efc3b3322721a580a66f92ae220438b530
Kent <kent@Davids-MacBook-Pro.local>
2019-02-03 15:06:09 +0400

    cont. passding values

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,16 +8,11 @@ class Counter extends Component {
   // <React.Fragment> = avoid duplicate divs
 
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.value
   };
 
-  product = { "name:": "Jason" };
-
-  handleIncrement = product => {
-    // this will error
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
@@ -25,33 +20,25 @@ class Counter extends Component {
     return (
       <div>
         <span className={this.getBadgeClass()}>{this.formatCount()}</span>
-        {/* onclick uses handleIncrement NOT handleIncrement() */}
         <button
-          // eslint-disable-next-line no-undef
-          onClick={() => this.handleIncrement(this.product)}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        {/* <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul> */}
       </div>
     );
   }
 
   getBadgeClass() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    // const x = <h1>Zero</h1>;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
