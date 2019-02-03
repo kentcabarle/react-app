e25ddda8b5dbad0793bc1c064c679f8a933fdc33
Kent <kent@Davids-MacBook-Pro.local>
2019-02-03 19:51:13 +0400

    Object destructuring

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,13 +3,13 @@ import React from "react";
 // stateless functional component
 // shortcut: sfc
 
-const NavBar = props => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar
         <span className="badge badge-pill badge-secondary m-2">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
